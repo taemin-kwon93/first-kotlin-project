4b856cd87baa84977887da49a67c320f3e5adf66
taemin-kwon93 <4146kt@gmail.com>
2022-11-28 21:58:24 +0900

    study: String and its related Operations
    - String Interpolation
    - Multiline Strings using TripleQuotes

--- src/main/kotlin/kotlinplayground/HelloKotlin.kt
+++ src/main/kotlin/kotlinplayground/HelloKotlin.kt
@@ -5,6 +5,30 @@ class HelloKotlin {
 
 fun main(args: Array<String>){
 
-    println("Hello Kotlin1")
+    val introduce: String = "this is Tom";
+    println("Hello $introduce") // Hello this is Tom
+    println("Hello ${introduce.length}") // Hello 11
 
+    val multiLine1 = "\nABC\nDEF\n";
+    println(multiLine1);
+    /** console
+     * ABC
+     * DEF
+     */
+
+    val multiLine2 = """
+        ABC
+        DEF
+        GHI
+        $introduce
+        ${introduce.length}
+    """.trimIndent()
+    println(multiLine2)
+    /** console
+     * ABC
+     * DEF
+     * GHI
+     * this is Tom
+     * 11
+     */
 }
